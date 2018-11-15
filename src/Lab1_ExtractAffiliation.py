29c8ad1e1c707794db7d21e13bf94061acf1816a
Charles Huang <chiyuanh@microsoft.com>
2018-11-14 20:47:51 -0800

    Update Lab1_ExtractAffiliation.py

--- src/Lab1_ExtractAffiliation.py
+++ src/Lab1_ExtractAffiliation.py
@@ -2,32 +2,47 @@ from __future__ import print_function
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import *
 
+# Replace containerName and accountName
+containerName = "myContainerName"
+accountName = "myAccountName"
+
+outputDir = "/output/user01/pyspark"
+
 if __name__ == "__main__":
-    spark = SparkSession\
-        .builder\
-        .appName("Microsoft academic graph spark Labs")\
+
+    # Start Spark context
+    spark = SparkSession \
+        .builder \
+        .appName("Microsoft academic graph spark Labs") \
         .getOrCreate()
     sqlContext = SQLContext(spark)
 
-    df = sqlContext.read.format('csv').\
-         option("delimiter", "\t").\
-         options(header='false', inferSchema='false').\
-         load('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/Affiliations.txt')
+    # Load Affiliations data
+    df = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load('wasbs://%s@%s.blob.core.windows.net/mag/Affiliations.txt' % (containerName, accountName))
 
+    # Insert headers
     headers = ['AffiliationId', 'Rank', 'NormalizedName', \
                'DisplayName', 'GridId', 'OfficialPage', 'WikiPage', \
                'PaperCount', 'CitationCount', 'CreatedDate']
     df = df.toDF(*headers)
 
-    # Have done: Start the Spark context, Load the Data and insert the schema as instructed
+    # Optional: peek the result
+    df.show()
+
     # Extract the AffiliationId for Microsoft
     microsoft = df.where(df.NormalizedName == 'microsoft').select(df.AffiliationId, df.DisplayName)
+
     # Optional: peek the result
     microsoft.show()
-    # Optional: Count how many rows in the result
-    print("Number of rows in the dataframe: {}".format(microsoft.count()))
-    microsoft.write.csv('/output/user01/pyspark/Affiliation.csv', \
-                        mode='overwrite', header='true')
-    # Stop the Spark context
 
+    # Optional: Count number of rows in result
+    print("Number of rows in the dataframe: {}".format(microsoft.count()))
+
+    # Output result
+    microsoft.write.csv('%s/Affiliation.csv' % outputDir, mode='overwrite', header='true')
+
+    # Stop Spark context
     spark.stop()
