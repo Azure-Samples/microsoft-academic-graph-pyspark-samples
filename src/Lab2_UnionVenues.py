eab3960c9d374e260b094bcd15a4f9654adf2192
Charles Huang <chiyuanh@microsoft.com>
2018-11-14 20:49:06 -0800

    Update Lab2_UnionVenues.py

--- src/Lab2_UnionVenues.py
+++ src/Lab2_UnionVenues.py
@@ -2,39 +2,68 @@ from __future__ import print_function
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import *
 
+# Replace containerName and accountName
+containerName = "myContainerName"
+accountName = "myAccountName"
+
+outputDir = "/output/user01/pyspark"
+
 if __name__ == "__main__":
-    spark = SparkSession\
-        .builder\
-        .appName("Microsoft academic graph spark Labs")\
+
+    # Start Spark context
+    spark = SparkSession \
+        .builder \
+        .appName("Microsoft academic graph spark Labs") \
         .getOrCreate()
     sqlContext = SQLContext(spark)
 
-    conferences = sqlContext.read.format('csv').\
-            option("delimiter", "\t").\
-            options(header='false', inferSchema='false').\
-            load('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/ConferenceSeries.txt')
+    #Load ConferenceSeries data
+    conferences = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load('wasbs://%s@%s.blob.core.windows.net/mag/ConferenceSeries.txt' % (containerName, accountName))
+
+    # Insert headers
     conferenceHeaders = ['ConferenceSeriesId', 'Rank', 'NormalizedName', \
-                            'DisplayName', 'PaperCount', 'CitationCount', 'CreatedDate']
+                         'DisplayName', 'PaperCount', 'CitationCount', 'CreatedDate']
     conferences = conferences.toDF(*conferenceHeaders)
 
-    journals = sqlContext.read.format('csv').\
-            option("delimiter", "\t").\
-            options(header='false', inferSchema='false').\
-            load('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/Journals.txt')
+    # Optional: peek result
+    conferences.show()
+
+    #Load Journals data
+    journals = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load('wasbs://%s@%s.blob.core.windows.net/mag/Journals.txt' % (containerName, accountName))
+
+    # Insert headers
     journalHeaders = ['JournalId', 'Rank', 'NormalizedName', \
-                            'DisplayName', 'Issn', 'Publisher', 'Webpage', \
-                            'PaperCount', 'CitationCount', 'CreatedDate']
+                      'DisplayName', 'Issn', 'Publisher', 'Webpage', \
+                      'PaperCount', 'CitationCount', 'CreatedDate']
     journals = journals.toDF(*journalHeaders)
 
-    conferences = conferences\
-                    .select(conferences.ConferenceSeriesId, conferences.DisplayName, conferences.NormalizedName)\
-                    .selectExpr('ConferenceSeriesId as VId', 'DisplayName as VenueName', 'NormalizedName as VenueShortName')
-    journals = journals\
-                .select(journals.JournalId, journals.DisplayName, journals.NormalizedName)\
-                .selectExpr('JournalId as VId', 'DisplayName as VenueName', 'NormalizedName as VenueShortName')
-    venue = conferences.union(journals)
-    venue.show()
-    print('Number of rows in venue: {}'.format(venue.count()))
-    venue.write.csv('/output/user01/pyspark/Venue.csv')
+    # Optional: peek result
+    journals.show()
 
+    conferences = conferences \
+        .select(conferences.ConferenceSeriesId, conferences.DisplayName, conferences.NormalizedName) \
+        .selectExpr('ConferenceSeriesId as VId', 'DisplayName as VenueName', 'NormalizedName as VenueShortName')
+
+    journals = journals \
+        .select(journals.JournalId, journals.DisplayName, journals.NormalizedName) \
+        .selectExpr('JournalId as VId', 'DisplayName as VenueName', 'NormalizedName as VenueShortName')
+
+    venue = conferences.union(journals)
+
+    # Optional: peek result
+    venue.show()
+
+    # Optional: Count number of rows in result
+    print('Number of rows in venue: {}'.format(venue.count()))
+
+    # Output result
+    venue.write.csv('%s/Venue.csv' % outputDir, mode='overwrite', header='true')
+
+    # Stop Spark context
     spark.stop()
