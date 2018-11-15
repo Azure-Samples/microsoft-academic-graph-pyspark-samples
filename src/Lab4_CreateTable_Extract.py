2dad9ec4e9a6c0a909e05ed0e916c15b98e0f166
Charles Huang <chiyuanh@microsoft.com>
2018-11-14 20:50:04 -0800

    Update Lab4_CreateTable_Extract.py

--- src/Lab4_CreateTable_Extract.py
+++ src/Lab4_CreateTable_Extract.py
@@ -3,62 +3,84 @@ from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.functions import concat, lit, log, when
 from pyspark.sql.types import *
 
+# Replace containerName and accountName
+containerName = "myContainerName"
+accountName = "myAccountName"
+
+outputDir = "/output/user01/pyspark"
+
 if __name__ == "__main__":
-    spark = SparkSession\
-        .builder\
-        .appName("academic graph spart test")\
+
+    # Start Spark context
+    spark = SparkSession \
+        .builder \
+        .appName("Microsoft academic graph spark Labs") \
         .getOrCreate()
     sqlContext = SQLContext(spark)
 
-    authors = sqlContext.read.format('csv').\
-            option("delimiter", "\t").\
-            options(header='false', inferSchema='false').\
-            load('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/Authors.txt')
-    authorHeaders = ['AuthorId', 'Rank', 'NormalizedName', 'DisplayName', \
-                        'LastKnownAffiliationId', 'PaperCount', 'CitationCount', 'CreatedDate']
-    authors = authors.toDF(*authorHeaders)
-    Author = authors
+    #Load Authors data
+    authors = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load('wasbs://%s@%s.blob.core.windows.net/mag/Authors.txt' % (containerName, accountName))
 
-    paperAuthorAffiliation = sqlContext.read.format('csv').\
-            option("delimiter", ",").\
-            options(header='true', inferSchema='true').\
-            load('/output/user01/pyspark/paperAuthorAffiliationRelationship.csv')
+    # Insert headers
+    authorHeaders = ['AuthorId', 'Rank', 'NormalizedName', 'DisplayName', \
+                     'LastKnownAffiliationId', 'PaperCount', 'CitationCount', 'CreatedDate']
+    authors = authors.toDF(*authorHeaders)
+
+    #Load PaperAuthorAffiliationRelationship data
+    paperAuthorAffiliation = sqlContext.read.format('csv') \
+        .option("delimiter", ",") \
+        .options(header='true', inferSchema='true') \
+        .load('%s/PaperAuthorAffiliationRelationship.csv' % outputDir)
 
     orgAuthorIds = paperAuthorAffiliation.select(paperAuthorAffiliation.AuthorId).distinct()
 
     # Get all author details.
-    orgAuthors = Author.join(orgAuthorIds, Author.AuthorId == orgAuthorIds.AuthorId, 'inner')\
-                        .select(orgAuthorIds.AuthorId, Author.DisplayName)\
-                        .selectExpr('AuthorId as AuthorId', 'DisplayName as AuthorName')
-    orgAuthors.show()
-    orgAuthors.write.csv('/output/user01/pyspark/Author.csv', \
-                            mode='overwrite', \
-                            header='true')
+    orgAuthors = authors.join(orgAuthorIds, authors.AuthorId == orgAuthorIds.AuthorId, 'inner') \
+        .select(orgAuthorIds.AuthorId, authors.DisplayName) \
+        .selectExpr('AuthorId as AuthorId', 'DisplayName as AuthorName')
 
-    papers = sqlContext.read.format('csv').\
-            option("delimiter", "\t").\
-            options(header='false', inferSchema='false').\
-            load('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/Papers.txt')
+    # Optional: peek result
+    orgAuthors.show()
+
+    # Output result
+    orgAuthors.write.csv('%s/Author.csv' % outputDir, mode='overwrite', header='true')
+
+    #Load Papers data
+    papers = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load('wasbs://%s@%s.blob.core.windows.net/mag/Papers.txt' % (containerName, accountName))
+
+    # Insert headers
     paperHeaders = ['PaperId', 'Rank', 'Doi', 'DocType', 'PaperTitle', 'OriginalTitle', \
                     'BookTitle', 'Year', 'Date', 'Publisher', 'JournalId', 'ConferenceSeriesId', \
                     'ConferenceInstanceId', 'Volume', 'Issue', 'FirstPage', 'LastPage', \
                     'ReferenceCount', 'CitationCount', 'EstimatedCitation', 'CreatedDate']
     papers = papers.toDF(*paperHeaders)
+
     Paper = papers.withColumn('Prefix', lit('https://academic.microsoft.com/#/detail/'))
 
     # Get all paper details.
     orgPaperIds = paperAuthorAffiliation.select(paperAuthorAffiliation.PaperId).distinct()
-    orgPapers = Paper.join(orgPaperIds, Paper.PaperId == orgPaperIds.PaperId)\
-                        .where(Paper.Year >= 1991)\
-                        .select(Paper.PaperId, Paper.PaperTitle.alias('Title'), Paper.EstimatedCitation.alias('CitationCount'), \
-                                Paper.Date, when(Paper.DocType.isNull(), 'Not available').otherwise(Paper.DocType).alias('PublicationType'), \
-                                log(Paper.Rank).alias('LogProb'), concat(Paper.Prefix, Paper.PaperId).alias('Url'), \
-                                when(Paper.ConferenceSeriesId.isNull(), Paper.JournalId).otherwise(Paper.ConferenceSeriesId).alias('VId'), \
-                                Paper.Year)
-    orgPapers.show()
-    print('Number of rows in orgPapers: {}'.format(orgPapers.count()))
-    orgPapers.write.csv('/output/user01/pyspark/Paper.csv', \
-                        mode='overwrite', \
-                        header='true')
+    orgPapers = Paper.join(orgPaperIds, Paper.PaperId == orgPaperIds.PaperId) \
+        .where(Paper.Year >= 1991) \
+        .select(Paper.PaperId, Paper.PaperTitle.alias('Title'), Paper.EstimatedCitation.alias('CitationCount'), \
+                Paper.Date, when(Paper.DocType.isNull(), 'Not available').otherwise(Paper.DocType).alias('PublicationType'), \
+                log(Paper.Rank).alias('LogProb'), concat(Paper.Prefix, Paper.PaperId).alias('Url'), \
+                when(Paper.ConferenceSeriesId.isNull(), Paper.JournalId).otherwise(Paper.ConferenceSeriesId).alias('VId'), \
+                Paper.Year)
 
+    # Optional: peek result
+    orgPapers.show()
+
+    # Optional: Count number of rows in result
+    print('Number of rows in orgPapers: {}'.format(orgPapers.count()))
+
+    # Output result
+    orgPapers.write.csv('%s/Paper.csv' % outputDir, mode='overwrite', header='true')
+
+    # Stop Spark context
     spark.stop()
