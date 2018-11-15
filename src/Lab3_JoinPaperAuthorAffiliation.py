2374b1c8bafc845e0b62dc91aa715e6feb05b969
Charles Huang <chiyuanh@microsoft.com>
2018-11-14 20:49:33 -0800

    Update Lab3_JoinPaperAuthorAffiliation.py

--- src/Lab3_JoinPaperAuthorAffiliation.py
+++ src/Lab3_JoinPaperAuthorAffiliation.py
@@ -2,33 +2,53 @@ from __future__ import print_function
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import *
 
+# Replace containerName and accountName
+containerName = "myContainerName"
+accountName = "myAccountName"
+
+outputDir = "/output/user01/pyspark"
+
 if __name__ == "__main__":
-    spark = SparkSession\
-        .builder\
-        .appName("academic graph spart test")\
+
+    # Start Spark context
+    spark = SparkSession \
+        .builder \
+        .appName("Microsoft academic graph spark Labs") \
         .getOrCreate()
     sqlContext = SQLContext(spark)
 
-    affiliations = sqlContext.read.format('csv').\
-         option("delimiter", ",").\
-         options(header='true', inferSchema='true').\
-         load('/output/user01/pyspark/Affiliation.csv')
+    # Load Affiliation data
+    affiliations = sqlContext.read.format('csv') \
+        .option("delimiter", ",") \
+        .options(header='true', inferSchema='true') \
+        .load('%s/Affiliation.csv' % outputDir)
 
-    paperAuthorAffiliations = sqlContext.read.format('csv').\
-         option("delimiter", "\t").\
-         options(header='false', inferSchema='false').\
-         load('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/PaperAuthorAffiliations.txt')
+    # Load PaperAuthorAffiliations data
+    paperAuthorAffiliations = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load('wasbs://%s@%s.blob.core.windows.net/mag/PaperAuthorAffiliations.txt' % (containerName, accountName))
+
+    # Insert headers
     paperAuthorAffiliationHeaders = ['PaperId', 'AuthorId', 'AffiliationId', 'AuthorSequenceNumber', 'OriginalAffiliation']
     paperAuthorAffiliations = paperAuthorAffiliations.toDF(*paperAuthorAffiliationHeaders)
 
-    orgPaperAuthorAffiliation = paperAuthorAffiliations\
-                                .join(affiliations, paperAuthorAffiliations.AffiliationId == affiliations.AffiliationId, 'inner')\
-                                .select(paperAuthorAffiliations.PaperId, paperAuthorAffiliations.AuthorId, \
-                                        affiliations.AffiliationId, paperAuthorAffiliations.AuthorSequenceNumber)
-    orgPaperAuthorAffiliation.show()
-    print('Number of rows in PaperAuthorAffiliation: {}'.format(orgPaperAuthorAffiliation.count()))
-    orgPaperAuthorAffiliation.write.csv('/output/user01/pyspark/paperAuthorAffiliationRelationship.csv', \
-                                         mode='overwrite', \
-                                         header='true')
+    # Optional: peek result
+    paperAuthorAffiliations.show()
 
+    orgPaperAuthorAffiliation = paperAuthorAffiliations \
+        .join(affiliations, paperAuthorAffiliations.AffiliationId == affiliations.AffiliationId, 'inner') \
+        .select(paperAuthorAffiliations.PaperId, paperAuthorAffiliations.AuthorId, \
+                affiliations.AffiliationId, paperAuthorAffiliations.AuthorSequenceNumber)
+
+    # Optional: peek result
+    orgPaperAuthorAffiliation.show()
+
+    # Optional: Count number of rows in result
+    print('Number of rows in PaperAuthorAffiliation: {}'.format(orgPaperAuthorAffiliation.count()))
+
+    # Output result
+    orgPaperAuthorAffiliation.write.csv('%s/PaperAuthorAffiliationRelationship.csv' % outputDir, mode='overwrite', header='true')
+
+    # Stop Spark context
     spark.stop()
