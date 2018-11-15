909e7ca560bf26211e217a5b7491409192e85268
Charles Huang <chiyuanh@microsoft.com>
2018-11-14 20:51:29 -0800

    Update Lab7_GroupByYear.py

--- src/Lab7_GroupByYear.py
+++ src/Lab7_GroupByYear.py
@@ -3,26 +3,37 @@ from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.functions import count, sum
 from pyspark.sql.types import *
 
+# Replace containerName and accountName
+containerName = "myContainerName"
+accountName = "myAccountName"
+
+outputDir = "/output/user01/pyspark"
+
 if __name__ == "__main__":
-    spark = SparkSession\
-        .builder\
-        .appName("Microsoft academic graph spark Labs")\
+
+    # Start Spark context
+    spark = SparkSession \
+        .builder \
+        .appName("Microsoft academic graph spark Labs") \
         .getOrCreate()
     sqlContext = SQLContext(spark)
 
     # Get all paper details for the input organization.
-    orgPapers = sqlContext.read.format('csv')\
-                .option("delimiter", ",")\
-                .options(header='true', inferSchema='true')\
-                .load('/output/user01/pyspark/Paper.csv')
+    orgPapers = sqlContext.read.format('csv') \
+        .option("delimiter", ",") \
+        .options(header='true', inferSchema='true') \
+        .load('%s/Paper.csv' % outputDir)
 
     # Get paper count and citation sum for each year.
-    #orgPaperGroupByYear = orgPapers.select(orgPapers.Year, count(orgPapers.PaperId).alias('PaperCount'), sum(orgPapers.CitationCount).alias('CitationSum'))\
-    #                        .groupBy(orgPapers.Year)
-    orgPaperGroupByYear = orgPapers.groupBy(orgPapers.Year).agg(count(orgPapers.PaperId).alias('PaperCount'), sum(orgPapers.CitationCount).alias('CitationSum'))
-    orgPaperGroupByYear.show()
-    orgPaperGroupByYear.write.csv('/output/user01/pyspark/OrgPaperGroupByYear.csv', \
-                                  mode='overwrite', \
-                                  header='true')
+    orgPaperGroupByYear = orgPapers \
+        .groupBy(orgPapers.Year) \
+        .agg(count(orgPapers.PaperId).alias('PaperCount'), sum(orgPapers.CitationCount).alias('CitationSum'))
 
+    # Optional: peek result
+    orgPaperGroupByYear.show()
+
+    # Output result
+    orgPaperGroupByYear.write.csv('%s/OrgPaperGroupByYear.csv' % outputDir, mode='overwrite', header='true')
+
+    # Stop Spark context
     spark.stop()
