2d6ae65f46445a547e32476d8edec32796d80048
Charles Huang <chiyuanh@microsoft.com>
2018-11-15 09:47:41 -0800

    Cyhuang01 sample1 (#1)
    * Update Lab1_ExtractAffiliation.py
    
    * Update Lab2_UnionVenues.py
    
    * Update Lab3_JoinPaperAuthorAffiliation.py
    
    * Update Lab4_CreateTable_Extract.py
    
    * Update Lab5_CreateTableByTvf.py
    
    * Update Lab6_GetPartnerData.py
    
    * Update Lab7_GroupByYear.py

--- src/Lab5_CreateTableByTvf.py
+++ src/Lab5_CreateTableByTvf.py
@@ -2,57 +2,75 @@ from __future__ import print_function
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import *
 
+# Replace containerName and accountName
+containerName = "myContainerName"
+accountName = "myAccountName"
+
+outputDir = "/output/user01/pyspark"
+
 def FieldsOfStudy(baseDir):
-    _fieldsOfStudy = sqlContext.read.format('csv')\
-                    .option("delimiter", "\t")\
-                    .options(header='false', inferSchema='false')\
-                    .load(baseDir + 'FieldsOfStudy.txt')
+    _fieldsOfStudy = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load(baseDir + 'FieldsOfStudy.txt')
+
     headers = ['FieldOfStudyId', 'Rank', 'NormalizedName', 'DisplayName', \
                'MainType', 'Level', 'PaperCount', 'CitationCount', 'CreatedDate']
     _fieldsOfStudy = _fieldsOfStudy.toDF(*headers)
     return _fieldsOfStudy
 
 def PaperFieldsOfStudy(baseDir):
-    _paperFiledsOfStudy = sqlContext.read.format('csv')\
-                         .option("delimiter", "\t")\
-                         .options(header='false', inferSchema='false')\
-                         .load(baseDir + 'PaperFieldsOfStudy.txt')
+    _paperFiledsOfStudy = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load(baseDir + 'PaperFieldsOfStudy.txt')
+
     headers = ['PaperId', 'FieldOfStudyId', 'Similarity']
     return _paperFiledsOfStudy.toDF(*headers)
 
 if __name__ == "__main__":
-    spark = SparkSession\
-        .builder\
-        .appName("academic graph spart test")\
+
+    # Start Spark context
+    spark = SparkSession \
+        .builder \
+        .appName("Microsoft academic graph spark Labs") \
         .getOrCreate()
     sqlContext = SQLContext(spark)
 
-    fieldOfStudy = FieldsOfStudy('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/')
-    paperFieldsOfStudy = PaperFieldsOfStudy('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/')
+    # Load FieldsOfStudy data
+    fieldOfStudy = FieldsOfStudy('wasbs://%s@%s.blob.core.windows.net/mag/' % (containerName, accountName))
+
+    # Load PaperFieldsOfStudy data
+    paperFieldsOfStudy = PaperFieldsOfStudy('wasbs://%s@%s.blob.core.windows.net/mag/' % (containerName, accountName))
 
     # Get all paper details for the input organization.
-    orgPapers = sqlContext.read.format('csv')\
-                .option("delimiter", ",")\
-                .options(header='true', inferSchema='true')\
-                .load('/output/user01/pyspark/Paper.csv')
+    orgPapers = sqlContext.read.format('csv') \
+        .option("delimiter", ",") \
+        .options(header='true', inferSchema='true') \
+        .load('%s/Paper.csv' % outputDir) 
 
     # Get all Paper-Field-of-Study relationships for the input organization.
-    orgPaperFieldOfStudy = paperFieldsOfStudy.join(orgPapers, paperFieldsOfStudy.PaperId == orgPapers.PaperId, 'inner')\
-                            .select(orgPapers.PaperId, paperFieldsOfStudy.FieldOfStudyId)
+    orgPaperFieldOfStudy = paperFieldsOfStudy.join(orgPapers, paperFieldsOfStudy.PaperId == orgPapers.PaperId, 'inner') \
+        .select(orgPapers.PaperId, paperFieldsOfStudy.FieldOfStudyId)
+
+    # Optional: peek result
     orgPaperFieldOfStudy.show()
-    orgPaperFieldOfStudy.write.csv('/output/user01/pyspark/PaperFieldOfStudyRelationship.csv', \
-                                    mode='overwrite', \
-                                    header='true')
+
+    # Output result
+    orgPaperFieldOfStudy.write.csv('%s/PaperFieldOfStudyRelationship.csv' % outputDir, mode='overwrite', header='true')
 
     # Get all field-of-study Ids for the input organization.
     orgFieldOfStudyIds = orgPaperFieldOfStudy.select(orgPaperFieldOfStudy.FieldOfStudyId).distinct()
 
     # Get all field-of-study details for the input organization
-    out_filedOfStudy = fieldOfStudy.join(orgFieldOfStudyIds, fieldOfStudy.FieldOfStudyId == orgFieldOfStudyIds.FieldOfStudyId, 'inner')\
-                        .select(orgFieldOfStudyIds.FieldOfStudyId, fieldOfStudy.Level.alias('FieldLevel'), fieldOfStudy.DisplayName.alias('FieldName'))
-    out_filedOfStudy.show()
-    out_filedOfStudy.write.csv('/output/user01/pyspark/FieldOfStudy.csv', \
-                                    mode='overwrite', \
-                                    header='true')
+    out_filedOfStudy = fieldOfStudy.join(orgFieldOfStudyIds, fieldOfStudy.FieldOfStudyId == orgFieldOfStudyIds.FieldOfStudyId, 'inner') \
+        .select(orgFieldOfStudyIds.FieldOfStudyId, fieldOfStudy.Level.alias('FieldLevel'), fieldOfStudy.DisplayName.alias('FieldName'))
 
+    # Optional: peek result
+    out_filedOfStudy.show()
+
+    # Output result
+    out_filedOfStudy.write.csv('%s/FieldOfStudy.csv' % outputDir, mode='overwrite', header='true')
+
+    # Stop Spark context
     spark.stop()
--- src/Lab6_GetPartnerData.py
+++ src/Lab6_GetPartnerData.py
@@ -2,83 +2,94 @@ from __future__ import print_function
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import *
 
+# Replace containerName and accountName
+containerName = "myContainerName"
+accountName = "myAccountName"
+
+outputDir = "/output/user01/pyspark"
+
+
 if __name__ == "__main__":
-    spark = SparkSession\
-        .builder\
-        .appName("academic graph spart test")\
+
+    # Start Spark context
+    spark = SparkSession \
+        .builder \
+        .appName("academic graph spart test") \
         .getOrCreate()
     sqlContext = SQLContext(spark)
 
     # Get all paper-author-affiliation relationships
-    paperAuthorAffiliations = sqlContext.read.format('csv')\
-                                .option("delimiter", "\t")\
-                                .options(header='false', inferSchema='false')\
-                                .load('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/PaperAuthorAffiliations.txt')
-    paperAuthorAffiliationHeaders = ['PaperId', 'AuthorId', 'AffiliationId', 'AuthorSequenceNumber']
+    paperAuthorAffiliations = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load('wasbs://%s@%s.blob.core.windows.net/mag/PaperAuthorAffiliations.txt' % (containerName, accountName))
+
+    paperAuthorAffiliationHeaders = ['PaperId', 'AuthorId', 'AffiliationId', 'AuthorSequenceNumber', 'OriginalAffiliation']
     paperAuthorAffiliations = paperAuthorAffiliations.toDF(*paperAuthorAffiliationHeaders)
 
     # Get all affiliation details.
-    affiliations = sqlContext.read.format('csv')\
-                    .option("delimiter", "\t")\
-                    .options(header='false', inferSchema='false')\
-                    .load('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/Affiliations.txt')
+    affiliations = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load('wasbs://%s@%s.blob.core.windows.net/mag/Affiliations.txt' % (containerName, accountName))
+
     affiliationHeaders = ['AffiliationId', 'Rank', 'NormalizedName', \
                           'DisplayName', 'GridId', 'OfficialPage', 'WikiPage', \
                           'PaperCount', 'CitationCount', 'CreatedDate']
     affiliations = affiliations.toDF(*affiliationHeaders)
 
     # Get all author details.
-    authors = sqlContext.read.format('csv')\
-                .option("delimiter", "\t")\
-                .options(header='false', inferSchema='false')\
-                .load('wasbs://mag-2018-09-27@magtrainingsource.blob.core.windows.net/mag/Authors.txt')
+    authors = sqlContext.read.format('csv') \
+        .option("delimiter", "\t") \
+        .options(header='false', inferSchema='false') \
+        .load('wasbs://%s@%s.blob.core.windows.net/mag/Authors.txt' % (containerName, accountName))
+
     authorHeaders = ['AuthorId', 'Rank', 'NormalizedName', 'DisplayName', \
                      'LastKnownAffiliationId', 'PaperCount', 'CitationCount', 'CreatedDate']
     authors = authors.toDF(*authorHeaders)
 
     # Get all paper details for the input organization.
-    orgPapers = sqlContext.read.format('csv')\
-                .option("delimiter", ",")\
-                .options(header='true', inferSchema='true')\
-                .load('/output/user01/pyspark/Paper.csv')
+    orgPapers = sqlContext.read.format('csv') \
+        .option("delimiter", ",") \
+        .options(header='true', inferSchema='true') \
+        .load('%s/Paper.csv' % outputDir)
 
     # Get all Paper-Author-Affiliation relationships for the input organization
-    orgpaperAuthorAffiliation = sqlContext.read.format('csv')\
-                            .option("delimiter", ",")\
-                            .options(header='true', inferSchema='true')\
-                            .load('/output/user01/pyspark/paperAuthorAffiliationRelationship.csv')
+    orgpaperAuthorAffiliation = sqlContext.read.format('csv') \
+        .option("delimiter", ",") \
+        .options(header='true', inferSchema='true') \
+        .load('%s/PaperAuthorAffiliationRelationship.csv' % outputDir)
 
     # Get all Paper-Author-Affiliation relationships for papers published by the input organization.
-    orgAllPaperAuthorAffiliations = paperAuthorAffiliations.join(orgPapers, paperAuthorAffiliations.PaperId == orgPapers.PaperId, 'inner')\
-                                        .select(orgPapers.PaperId, paperAuthorAffiliations.AuthorId, \
-                                                paperAuthorAffiliations.AffiliationId, paperAuthorAffiliations.AuthorSequenceNumber)
+    orgAllPaperAuthorAffiliations = paperAuthorAffiliations \
+        .join(orgPapers, paperAuthorAffiliations.PaperId == orgPapers.PaperId, 'inner') \
+        .select(orgPapers.PaperId, paperAuthorAffiliations.AuthorId, \
+                paperAuthorAffiliations.AffiliationId, paperAuthorAffiliations.AuthorSequenceNumber)
 
     # Get partner Paper-Author-Affiliation relationships by excluding those relationships of the input organization.
     orgPartnerPaperAuthorAffiliation = orgAllPaperAuthorAffiliations.subtract(orgpaperAuthorAffiliation)
     orgPartnerPaperAuthorAffiliation.show()
-    orgPartnerPaperAuthorAffiliation.write.csv('/output/user01/pyspark/Partner_PaperAuthorAffiliationRelationship.csv', \
-                                               mode='overwrite', \
-                                               header='true')
+    orgPartnerPaperAuthorAffiliation.write.csv('%s/Partner_PaperAuthorAffiliationRelationship.csv' % outputDir, mode='overwrite', header='true')
 
     # Get all partner affiliation Ids.
-    orgPartnerAffiliationIds = orgPartnerPaperAuthorAffiliation.where(orgPartnerPaperAuthorAffiliation.AffiliationId.isNotNull())\
-                                    .select(orgPartnerPaperAuthorAffiliation.AffiliationId).distinct()
+    orgPartnerAffiliationIds = orgPartnerPaperAuthorAffiliation \
+        .where(orgPartnerPaperAuthorAffiliation.AffiliationId.isNotNull()) \
+        .select(orgPartnerPaperAuthorAffiliation.AffiliationId) \
+        .distinct()
 
     # Get all partner affiliation details.
-    orgPartnerAffiliations = affiliations.join(orgPartnerAffiliationIds, affiliations.AffiliationId == orgPartnerAffiliationIds.AffiliationId, 'inner')\
-                                         .select(orgPartnerAffiliationIds.AffiliationId, affiliations.DisplayName.alias('AffiliationName'))
+    orgPartnerAffiliations = affiliations \
+        .join(orgPartnerAffiliationIds, affiliations.AffiliationId == orgPartnerAffiliationIds.AffiliationId, 'inner') \
+        .select(orgPartnerAffiliationIds.AffiliationId, affiliations.DisplayName.alias('AffiliationName'))
     orgPartnerAffiliations.show()
-    orgPartnerAffiliations.write.csv('/output/user01/pyspark/Partner_Affiliation.csv', \
-                                     mode='overwrite', \
-                                     header='true')
+    orgPartnerAffiliations.write.csv('%s/Partner_Affiliation.csv' % outputDir, mode='overwrite', header='true')
 
     # Get all partner author Ids.
     orgPartnerAuthorIds = orgPartnerPaperAuthorAffiliation.select(orgPartnerPaperAuthorAffiliation.AuthorId).distinct()
-    orgPartnerAuthors = authors.join(orgPartnerAuthorIds, orgPartnerAuthorIds.AuthorId == authors.AuthorId)\
+    orgPartnerAuthors = authors.join(orgPartnerAuthorIds, orgPartnerAuthorIds.AuthorId == authors.AuthorId) \
                             .select(orgPartnerAuthorIds.AuthorId, authors.DisplayName.alias('AuthorName'))
     orgPartnerAuthors.show()
-    orgPartnerAuthors.write.csv('/output/user01/pyspark/Partner_Author.csv', \
-                                 mode='overwrite', \
-                                 header='true')
+    orgPartnerAuthors.write.csv('%s/Partner_Author.csv' % outputDir, mode='overwrite', header='true')
 
+    # Stop Spark context
     spark.stop()
