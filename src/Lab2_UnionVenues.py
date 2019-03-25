d937486b134df1bdcb5fcd64de0b2bfa14bf440b
Charles Huang <chiyuanh@microsoft.com>
2019-03-25 15:30:12 -0700

    Update Lab2_UnionVenues.py

--- src/Lab2_UnionVenues.py
+++ src/Lab2_UnionVenues.py
@@ -1,52 +1,28 @@
-from __future__ import print_function
-from pyspark.sql import SparkSession, SQLContext
-from pyspark.sql.types import *
+#Load ConferenceSeries data
+conferences = getConferenceSeriesDataFrame(MagDir)
+# Optional: peek result
+conferences.show()
 
-from CreatePySparkFunctions import *
+#Load Journals data
+journals = getJournalsDataFrame(MagDir)
+# Optional: peek result
+journals.show()
 
-# Replace containerName and accountName
-containerName = "myContainerName"
-accountName = "myAccountName"
+conferences = conferences \
+    .select(conferences.ConferenceSeriesId, conferences.DisplayName, conferences.NormalizedName) \
+    .selectExpr('ConferenceSeriesId as VId', 'DisplayName as VenueName', 'NormalizedName as VenueShortName')
 
-outputDir = "/output/user01/pyspark"
+journals = journals \
+    .select(journals.JournalId, journals.DisplayName, journals.NormalizedName) \
+    .selectExpr('JournalId as VId', 'DisplayName as VenueName', 'NormalizedName as VenueShortName')
 
-if __name__ == "__main__":
+venue = conferences.union(journals)
 
-    # Start Spark context
-    spark = SparkSession \
-        .builder \
-        .appName("Microsoft academic graph spark Labs") \
-        .getOrCreate()
-    sqlContext = SQLContext(spark)
+# Optional: peek result
+venue.show()
 
-    #Load ConferenceSeries data
-    conferences = getDataFrameForConferenceSeries(sqlContext, containerName, accountName)
-    # Optional: peek result
-    conferences.show()
+# Optional: Count number of rows in result
+print('Number of rows in venue: {}'.format(venue.count()))
 
-    #Load Journals data
-    journals = getDataFrameForJournals(sqlContext, containerName, accountName)
-    # Optional: peek result
-    journals.show()
-
-    conferences = conferences \
-        .select(conferences.ConferenceSeriesId, conferences.DisplayName, conferences.NormalizedName) \
-        .selectExpr('ConferenceSeriesId as VId', 'DisplayName as VenueName', 'NormalizedName as VenueShortName')
-
-    journals = journals \
-        .select(journals.JournalId, journals.DisplayName, journals.NormalizedName) \
-        .selectExpr('JournalId as VId', 'DisplayName as VenueName', 'NormalizedName as VenueShortName')
-
-    venue = conferences.union(journals)
-
-    # Optional: peek result
-    venue.show()
-
-    # Optional: Count number of rows in result
-    print('Number of rows in venue: {}'.format(venue.count()))
-
-    # Output result
-    venue.write.csv('%s/Venue.csv' % outputDir, mode='overwrite', header='true')
-
-    # Stop Spark context
-    spark.stop()
+# Output result
+venue.coalesce(1).write.mode('overwrite').format('csv').option('header','true').save('%s/Venue.csv' % OutputDir)
