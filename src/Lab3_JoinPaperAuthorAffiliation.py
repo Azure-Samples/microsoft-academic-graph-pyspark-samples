f200797bc0bd6581eb069df1dc3e2d606f22edb8
Charles Huang <chiyuanh@microsoft.com>
2019-03-26 08:54:16 -0700

    Update Lab3_JoinPaperAuthorAffiliation.py

--- src/Lab3_JoinPaperAuthorAffiliation.py
+++ src/Lab3_JoinPaperAuthorAffiliation.py
@@ -1,22 +1,23 @@
-# Load Affiliation data
+# Load Affiliation data for the input organization from previous output
 header = ['AffiliationId', 'DisplayName']
 affiliations = spark.read.format('csv').options(header='true', inferSchema='true').load('%s/%s' % (OutputDir, 'Affiliation.csv')).toDF(*header)
 
 # Load PaperAuthorAffiliations data
 paperAuthorAffiliations = getPaperAuthorAffiliationsDataFrame(MagDir)
 # Optional: peek result
-paperAuthorAffiliations.show()
+paperAuthorAffiliations.show(10)
 
+# Filter PaperAuthorAffiliations by AffiliationId
 orgPaperAuthorAffiliation = paperAuthorAffiliations \
     .join(affiliations, paperAuthorAffiliations.AffiliationId == affiliations.AffiliationId, 'inner') \
     .select(paperAuthorAffiliations.PaperId, paperAuthorAffiliations.AuthorId, \
             affiliations.AffiliationId, paperAuthorAffiliations.AuthorSequenceNumber)
 
 # Optional: peek result
-orgPaperAuthorAffiliation.show()
+orgPaperAuthorAffiliation.show(10)
 
 # Optional: Count number of rows in result
 print('Number of rows in PaperAuthorAffiliation: {}'.format(orgPaperAuthorAffiliation.count()))
 
 # Output result
-orgPaperAuthorAffiliation.coalesce(1).write.mode('overwrite').format('csv').option('header','true').save('%s/PaperAuthorAffiliationRelationship.csv' % OutputDir)
+orgPaperAuthorAffiliation.write.mode('overwrite').format('csv').option('header','true').save('%s/PaperAuthorAffiliationRelationship.csv' % OutputDir)
