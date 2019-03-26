0934551c2d3a4cad58ac13b3773c1530fa98d6ee
Charles Huang <chiyuanh@microsoft.com>
2019-03-26 08:56:22 -0700

    Update Lab7_GroupByYear.py

--- src/Lab7_GroupByYear.py
+++ src/Lab7_GroupByYear.py
@@ -1,39 +1,16 @@
-from __future__ import print_function
-from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.functions import count, sum
-from pyspark.sql.types import *
 
-# Replace containerName and accountName
-containerName = "myContainerName"
-accountName = "myAccountName"
+# Get all paper details for the input organization from previous output
+header = ['PaperId', 'Title', 'CitationCount', 'Date', 'PublicationType', 'LogProb', 'Url', 'VId', 'Year']
+orgPapers = spark.read.format('csv').options(header='true', inferSchema='true').load('%s/%s' % (OutputDir, 'Paper.csv')).toDF(*header)
 
-outputDir = "/output/user01/pyspark"
+# Get paper count and citation sum for each year
+orgPaperGroupByYear = orgPapers \
+    .groupBy(orgPapers.Year) \
+    .agg(count(orgPapers.PaperId).alias('PaperCount'), sum(orgPapers.CitationCount).alias('CitationSum'))
 
-if __name__ == "__main__":
+# Optional: peek result
+orgPaperGroupByYear.show(10)
 
-    # Start Spark context
-    spark = SparkSession \
-        .builder \
-        .appName("Microsoft academic graph spark Labs") \
-        .getOrCreate()
-    sqlContext = SQLContext(spark)
-
-    # Get all paper details for the input organization.
-    orgPapers = sqlContext.read.format('csv') \
-        .option("delimiter", ",") \
-        .options(header='true', inferSchema='true') \
-        .load('%s/Paper.csv' % outputDir)
-
-    # Get paper count and citation sum for each year.
-    orgPaperGroupByYear = orgPapers \
-        .groupBy(orgPapers.Year) \
-        .agg(count(orgPapers.PaperId).alias('PaperCount'), sum(orgPapers.CitationCount).alias('CitationSum'))
-
-    # Optional: peek result
-    orgPaperGroupByYear.show()
-
-    # Output result
-    orgPaperGroupByYear.write.csv('%s/OrgPaperGroupByYear.csv' % outputDir, mode='overwrite', header='true')
-
-    # Stop Spark context
-    spark.stop()
+# Output result
+orgPaperGroupByYear.coalesce(1).write.mode('overwrite').format('csv').option('header','true').save('%s/PaperGroupByYear.csv' % OutputDir)
