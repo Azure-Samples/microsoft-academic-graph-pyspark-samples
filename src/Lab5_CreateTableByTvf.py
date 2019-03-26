18f85b3dc286a70c41291d0469b10b9c2e4850a5
Charles Huang <chiyuanh@microsoft.com>
2019-03-26 08:55:11 -0700

    Update Lab5_CreateTableByTvf.py

--- src/Lab5_CreateTableByTvf.py
+++ src/Lab5_CreateTableByTvf.py
@@ -1,32 +1,32 @@
+# Get paper details for the input organization from previous output
+header = ['PaperId', 'Title', 'CitationCount', 'Date', 'PublicationType', 'LogProb', 'Url', 'VId', 'Year']
+orgPapers = spark.read.format('csv').options(header='true', inferSchema='true').load('%s/%s' % (OutputDir, 'Paper.csv')).toDF(*header)
+
 # Load FieldsOfStudy data
 fieldOfStudy = getFieldsOfStudyDataFrame(MagDir)
 
 # Load PaperFieldsOfStudy data
 paperFieldsOfStudy = getPaperFieldsOfStudyDataFrame(MagDir)
 
-# Get all paper details for the input organization.
-header = ['PaperId', 'AuthorId', 'AffiliationId', 'AuthorSequenceNumber']
-orgPapers = spark.read.format('csv').options(header='true', inferSchema='true').load('%s/%s' % (OutputDir, 'Paper.csv')).toDF(*header)
-
-# Get all Paper-Field-of-Study relationships for the input organization.
+# Get Paper-Field-of-Study relationships for the input organization
 orgPaperFieldOfStudy = paperFieldsOfStudy.join(orgPapers, paperFieldsOfStudy.PaperId == orgPapers.PaperId, 'inner') \
     .select(orgPapers.PaperId, paperFieldsOfStudy.FieldOfStudyId)
 
 # Optional: peek result
-orgPaperFieldOfStudy.show()
+orgPaperFieldOfStudy.show(10)
 
 # Output result
-orgPaperFieldOfStudy.coalesce(1).write.mode('overwrite').format('csv').option('header','true').save('%s/PaperFieldOfStudyRelationship.csv' % OutputDir)
+orgPaperFieldOfStudy.write.mode('overwrite').format('csv').option('header','true').save('%s/PaperFieldOfStudyRelationship.csv' % OutputDir)
 
-# Get all field-of-study Ids for the input organization.
+# Get all field-of-study Ids for the input organization
 orgFieldOfStudyIds = orgPaperFieldOfStudy.select(orgPaperFieldOfStudy.FieldOfStudyId).distinct()
 
 # Get all field-of-study details for the input organization
-out_filedOfStudy = fieldOfStudy.join(orgFieldOfStudyIds, fieldOfStudy.FieldOfStudyId == orgFieldOfStudyIds.FieldOfStudyId, 'inner') \
+orgFiledOfStudy = fieldOfStudy.join(orgFieldOfStudyIds, fieldOfStudy.FieldOfStudyId == orgFieldOfStudyIds.FieldOfStudyId, 'inner') \
     .select(orgFieldOfStudyIds.FieldOfStudyId, fieldOfStudy.Level.alias('FieldLevel'), fieldOfStudy.DisplayName.alias('FieldName'))
 
 # Optional: peek result
-out_filedOfStudy.show()
+orgFiledOfStudy.show(10)
 
 # Output result
-out_filedOfStudy.coalesce(1).write.mode('overwrite').format('csv').option('header','true').save('%s/FieldOfStudy.csv' % OutputDir)
+orgFiledOfStudy.write.mode('overwrite').format('csv').option('header','true').save('%s/FieldOfStudy.csv' % OutputDir)
