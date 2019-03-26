f33653df83d5d4df24a77b8e90da05f10859f7f8
Charles Huang <chiyuanh@microsoft.com>
2019-03-26 15:05:21 -0700

    Update Lab0_Setup.py

--- src/Lab0_Setup.py
+++ src/Lab0_Setup.py
@@ -5,3 +5,21 @@ OutputContainer = '<OutputContainer>'             # The container name in Azure 
 
 MagDir = '/mnt/mag'
 OutputDir = '/mnt/output'
+
+if (any(mount.mountPoint == MagDir for mount in dbutils.fs.mounts())):
+  dbutils.fs.unmount(MagDir)
+
+dbutils.fs.mount(
+  source = ('wasbs://%s@%s.blob.core.windows.net' % (MagContainer, AzureStorageAccount)),
+  mount_point = MagDir,
+  extra_configs = {('fs.azure.account.key.%s.blob.core.windows.net' % AzureStorageAccount) : AzureStorageAccessKey})
+
+if (any(mount.mountPoint == OutputDir for mount in dbutils.fs.mounts())):
+  dbutils.fs.unmount(OutputDir)
+
+dbutils.fs.mount(
+  source = ('wasbs://%s@%s.blob.core.windows.net' % (OutputContainer, AzureStorageAccount)),
+  mount_point = OutputDir,
+  extra_configs = {('fs.azure.account.key.%s.blob.core.windows.net' % AzureStorageAccount) : AzureStorageAccessKey})
+
+dbutils.fs.ls('/mnt')
