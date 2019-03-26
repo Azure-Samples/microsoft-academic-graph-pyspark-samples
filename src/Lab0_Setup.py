cddd7a33d8c68bdb5a674f0f89e7aededd789e79
Charles Huang <chiyuanh@microsoft.com>
2019-03-26 09:10:53 -0700

    Create Lab0_Setup.py

---
+++ src/Lab0_Setup.py
@@ -0,0 +1,7 @@
+AzureStorageAccount = '<AzureStorageAccount>'     # Azure Storage (AS) account containing MAG dataset
+AzureStorageAccessKey = '<AzureStorageAccessKey>' # Access Key of the Azure Storage (AS) account
+MagContainer = '<MagContainer>'                   # The container name in Azure Storage (AS) account containing MAG dataset, Usually in forms of mag-yyyy-mm-dd
+OutputContainer = '<OutputContainer>'             # The container name in Azure Storage (AS) account where the output goes to
+
+MagDir = '/mnt/mag'
+OutputDir = '/mnt/output'
