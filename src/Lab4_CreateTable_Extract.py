e6fcbe20316821ada8c0ea39596b492924eeebf4
Charles Huang <chiyuanh@microsoft.com>
2019-03-26 08:54:43 -0700

    Update Lab4_CreateTable_Extract.py

--- src/Lab4_CreateTable_Extract.py
+++ src/Lab4_CreateTable_Extract.py
@@ -1,43 +1,48 @@
+from pyspark.sql.functions import concat, lit, log, when
+
 #Load Authors data
 authors = getAuthorsDataFrame(MagDir)
 
-#Load PaperAuthorAffiliationRelationship data
+#Load PaperAuthorAffiliationRelationship data from previous output
 header = ['PaperId', 'AuthorId', 'AffiliationId', 'AuthorSequenceNumber']
 paperAuthorAffiliation = spark.read.format('csv').options(header='true', inferSchema='true').load('%s/%s' % (OutputDir, 'PaperAuthorAffiliationRelationship.csv')).toDF(*header)
 
+paperAuthorAffiliation.show(10)
+
 orgAuthorIds = paperAuthorAffiliation.select(paperAuthorAffiliation.AuthorId).distinct()
 
-# Get all author details.
+# Get all author details
 orgAuthors = authors.join(orgAuthorIds, authors.AuthorId == orgAuthorIds.AuthorId, 'inner') \
     .select(orgAuthorIds.AuthorId, authors.DisplayName) \
     .selectExpr('AuthorId as AuthorId', 'DisplayName as AuthorName')
 
 # Optional: peek result
-orgAuthors.show()
+orgAuthors.show(10)
 
 # Output result
-orgAuthors.write.csv('%s/Author.csv' % outputDir, mode='overwrite', header='true')
+orgAuthors.write.mode('overwrite').format('csv').option('header','true').save('%s/Author.csv' % OutputDir)
 
 #Load Papers data
-papers = getDataFrameForPapers(sqlContext, containerName, accountName)
+papers = getPapersDataFrame(MagDir)
 
-Paper = papers.withColumn('Prefix', lit('https://academic.microsoft.com/#/detail/'))
+papers = papers.withColumn('Prefix', lit('https://academic.microsoft.com/#/detail/'))
 
-# Get all paper details.
+# Get all paper details
 orgPaperIds = paperAuthorAffiliation.select(paperAuthorAffiliation.PaperId).distinct()
-orgPapers = Paper.join(orgPaperIds, Paper.PaperId == orgPaperIds.PaperId) \
-    .where(Paper.Year >= 1991) \
-    .select(Paper.PaperId, Paper.PaperTitle.alias('Title'), Paper.EstimatedCitation.alias('CitationCount'), \
-            Paper.Date, when(Paper.DocType.isNull(), 'Not available').otherwise(Paper.DocType).alias('PublicationType'), \
-            log(Paper.Rank).alias('LogProb'), concat(Paper.Prefix, Paper.PaperId).alias('Url'), \
-            when(Paper.ConferenceSeriesId.isNull(), Paper.JournalId).otherwise(Paper.ConferenceSeriesId).alias('VId'), \
-            Paper.Year)
+
+orgPapers = papers.join(orgPaperIds, papers.PaperId == orgPaperIds.PaperId) \
+    .where(papers.Year >= 1991) \
+    .select(papers.PaperId, papers.PaperTitle.alias('Title'), papers.EstimatedCitation.alias('CitationCount'), \
+            papers.Date, when(papers.DocType.isNull(), 'Not available').otherwise(papers.DocType).alias('PublicationType'), \
+            log(papers.Rank).alias('LogProb'), concat(papers.Prefix, papers.PaperId).alias('Url'), \
+            when(papers.ConferenceSeriesId.isNull(), papers.JournalId).otherwise(papers.ConferenceSeriesId).alias('VId'), \
+            papers.Year)
 
 # Optional: peek result
-orgPapers.show()
+orgPapers.show(10)
 
 # Optional: Count number of rows in result
 print('Number of rows in orgPapers: {}'.format(orgPapers.count()))
 
 # Output result
-orgPapers.coalesce(1).write.mode('overwrite').format('csv').option('header','true').save('%s/Paper.csv' % OutputDir)
+orgPapers.write.mode('overwrite').format('csv').option('header','true').save('%s/Paper.csv' % OutputDir)
