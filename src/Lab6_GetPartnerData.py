dc37b8192593c75f80ff5a17ace48435e0e261ad
Charles Huang <chiyuanh@microsoft.com>
2019-03-26 08:55:59 -0700

    Update Lab6_GetPartnerData.py

--- src/Lab6_GetPartnerData.py
+++ src/Lab6_GetPartnerData.py
@@ -1,76 +1,47 @@
-from __future__ import print_function
-from pyspark.sql import SparkSession, SQLContext
-from pyspark.sql.types import *
+# Get all paper details for the input organization from previous output
+header = ['PaperId', 'Title', 'CitationCount', 'Date', 'PublicationType', 'LogProb', 'Url', 'VId', 'Year']
+orgPapers = spark.read.format('csv').options(header='true', inferSchema='true').load('%s/%s' % (OutputDir, 'Paper.csv')).toDF(*header)
 
-from CreatePySparkFunctions import *
+# Get all Paper-Author-Affiliation relationships for the input organization from previous output
+header = ['PaperId', 'AuthorId', 'AffiliationId', 'AuthorSequenceNumber']
+orgPaperAuthorAffiliation = spark.read.format('csv').options(header='true', inferSchema='true').load('%s/%s' % (OutputDir, 'PaperAuthorAffiliationRelationship.csv')).toDF(*header)
 
-# Replace containerName and accountName
-containerName = "myContainerName"
-accountName = "myAccountName"
+# Get all paper-author-affiliation relationships
+paperAuthorAffiliations = getPaperAuthorAffiliationsDataFrame(MagDir)
 
-outputDir = "/output/user01/pyspark"
+# Get all affiliation details
+affiliations = getAffiliationsDataFrame(MagDir)
 
+# Get all author details
+authors = getAuthorsDataFrame(MagDir)
 
-if __name__ == "__main__":
+# Get all Paper-Author-Affiliation relationships for papers published by the input organization
+orgAllPaperAuthorAffiliations = paperAuthorAffiliations \
+    .join(orgPapers, paperAuthorAffiliations.PaperId == orgPapers.PaperId, 'inner') \
+    .select(orgPapers.PaperId, paperAuthorAffiliations.AuthorId, \
+            paperAuthorAffiliations.AffiliationId, paperAuthorAffiliations.AuthorSequenceNumber)
 
-    # Start Spark context
-    spark = SparkSession \
-        .builder \
-        .appName("academic graph spart test") \
-        .getOrCreate()
-    sqlContext = SQLContext(spark)
+# Get partner Paper-Author-Affiliation relationships by excluding those relationships of the input organization
+partnerPaperAuthorAffiliation = orgAllPaperAuthorAffiliations.subtract(orgPaperAuthorAffiliation)
+partnerPaperAuthorAffiliation.show(10)
+partnerPaperAuthorAffiliation.write.mode('overwrite').format('csv').option('header','true').save('%s/PartnerPaperAuthorAffiliationRelationship.csv' % OutputDir)
 
-    # Get all paper-author-affiliation relationships
-    paperAuthorAffiliations = getDataFrameForPaperAuthorAffiliations(sqlContext, containerName, accountName)
+# Get all partner affiliation Ids
+partnerAffiliationIds = partnerPaperAuthorAffiliation \
+    .where(partnerPaperAuthorAffiliation.AffiliationId.isNotNull()) \
+    .select(partnerPaperAuthorAffiliation.AffiliationId) \
+    .distinct()
 
-    # Get all affiliation details.
-    affiliations = getDataFrameForAffiliations(sqlContext, containerName, accountName)
+# Get all partner affiliation details
+partnerAffiliations = affiliations \
+    .join(partnerAffiliationIds, affiliations.AffiliationId == partnerAffiliationIds.AffiliationId, 'inner') \
+    .select(partnerAffiliationIds.AffiliationId, affiliations.DisplayName.alias('AffiliationName'))
+partnerAffiliations.show(10)
+partnerAffiliations.write.mode('overwrite').format('csv').option('header','true').save('%s/PartnerAffiliation.csv' % OutputDir)
 
-    # Get all author details.
-    authors = getDataFrameForAuthors(sqlContext, containerName, accountName)
-
-    # Get all paper details for the input organization.
-    orgPapers = sqlContext.read.format('csv') \
-        .option("delimiter", ",") \
-        .options(header='true', inferSchema='true') \
-        .load('%s/Paper.csv' % outputDir)
-
-    # Get all Paper-Author-Affiliation relationships for the input organization
-    orgpaperAuthorAffiliation = sqlContext.read.format('csv') \
-        .option("delimiter", ",") \
-        .options(header='true', inferSchema='true') \
-        .load('%s/PaperAuthorAffiliationRelationship.csv' % outputDir)
-
-    # Get all Paper-Author-Affiliation relationships for papers published by the input organization.
-    orgAllPaperAuthorAffiliations = paperAuthorAffiliations \
-        .join(orgPapers, paperAuthorAffiliations.PaperId == orgPapers.PaperId, 'inner') \
-        .select(orgPapers.PaperId, paperAuthorAffiliations.AuthorId, \
-                paperAuthorAffiliations.AffiliationId, paperAuthorAffiliations.AuthorSequenceNumber)
-
-    # Get partner Paper-Author-Affiliation relationships by excluding those relationships of the input organization.
-    orgPartnerPaperAuthorAffiliation = orgAllPaperAuthorAffiliations.subtract(orgpaperAuthorAffiliation)
-    orgPartnerPaperAuthorAffiliation.show()
-    orgPartnerPaperAuthorAffiliation.write.csv('%s/Partner_PaperAuthorAffiliationRelationship.csv' % outputDir, mode='overwrite', header='true')
-
-    # Get all partner affiliation Ids.
-    orgPartnerAffiliationIds = orgPartnerPaperAuthorAffiliation \
-        .where(orgPartnerPaperAuthorAffiliation.AffiliationId.isNotNull()) \
-        .select(orgPartnerPaperAuthorAffiliation.AffiliationId) \
-        .distinct()
-
-    # Get all partner affiliation details.
-    orgPartnerAffiliations = affiliations \
-        .join(orgPartnerAffiliationIds, affiliations.AffiliationId == orgPartnerAffiliationIds.AffiliationId, 'inner') \
-        .select(orgPartnerAffiliationIds.AffiliationId, affiliations.DisplayName.alias('AffiliationName'))
-    orgPartnerAffiliations.show()
-    orgPartnerAffiliations.write.csv('%s/Partner_Affiliation.csv' % outputDir, mode='overwrite', header='true')
-
-    # Get all partner author Ids.
-    orgPartnerAuthorIds = orgPartnerPaperAuthorAffiliation.select(orgPartnerPaperAuthorAffiliation.AuthorId).distinct()
-    orgPartnerAuthors = authors.join(orgPartnerAuthorIds, orgPartnerAuthorIds.AuthorId == authors.AuthorId) \
-                            .select(orgPartnerAuthorIds.AuthorId, authors.DisplayName.alias('AuthorName'))
-    orgPartnerAuthors.show()
-    orgPartnerAuthors.write.csv('%s/Partner_Author.csv' % outputDir, mode='overwrite', header='true')
-
-    # Stop Spark context
-    spark.stop()
+# Get all partner author Ids
+partnerAuthorIds = partnerPaperAuthorAffiliation.select(partnerPaperAuthorAffiliation.AuthorId).distinct()
+partnerAuthors = authors.join(partnerAuthorIds, partnerAuthorIds.AuthorId == authors.AuthorId) \
+                        .select(partnerAuthorIds.AuthorId, authors.DisplayName.alias('AuthorName'))
+partnerAuthors.show(10)
+partnerAuthors.write.mode('overwrite').format('csv').option('header','true').save('%s/PartnerAuthor.csv' % OutputDir)
